0248847b56b9ed738dd258ff68b79b91ff18e290
jluo9612 <luojiabei@gmail.com>
2018-04-07 10:18:03 -0700

    4/7

---
+++ cpp/movezeroes.cpp
@@ -0,0 +1,10 @@
+void moveZeroes(vector<int>& nums) {        
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) nums[count++] = nums[i]; 
+        }
+        // now count is at the first index after all non-zero items
+        while (count < nums.size()) nums[count++] = 0;
+        
+}

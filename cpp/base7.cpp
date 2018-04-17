d95efeed714ccfd75de09587e2b1ddf0f88093fe
jluo9612 <luojiabei@gmail.com>
2018-04-16 17:44:45 -0700

    4/7

---
+++ cpp/base7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        string ans = "";
+        int cal = num;
+        if (num == 0) return "0";
+        if (num < 0) cal = 0-cal;
+        while (cal != 0) {
+            ans += to_string(cal%7);
+            cal /= 7;
+        }
+        reverse(ans.begin(), ans.end());
+        if (num < 0) ans.insert(ans.begin(), '-');
+        return ans;
+    }
+};
